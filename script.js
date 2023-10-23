a04d72d52654beb1c298bac23d1e2dff601cd20f
AlishaKausheen <alishakausheen@gmail.com>
2023-10-23 02:16:03 -0700

    functionalities done

--- script.js
+++ script.js
@@ -3,21 +3,34 @@ const pauseBtn = document.getElementById(`pause-btn`);
 const stopBtn = document.getElementById(`stop-btn`);
 const textInput = document.getElementById(`text`);
 const speedInput = document.getElementById(`speed`);
-
+let currentCharacter;
 playBtn.addEventListener(`click`, () => {
     playText(textInput.value);
 })
 pauseBtn.addEventListener(`click`, pauseText);
 stopBtn.addEventListener(`click`, stopText);
+speedInput.addEventListener(`input`, () => {
+    stopText();
+    playText(utterance.text.substring(currentCharacter))
+})
+
+const utterance = new SpeechSynthesisUtterance(text);
+    
+    utterance.addEventListener(`end`, () => {
+        textInput.disabled = false;
+    })
+    utterance.addEventListener(`boundary`, e => {
+        currentCharacter = e.charIndex;  
+    })
+
 function playText(text) {
     if (speechSynthesis.paused && speechSynthesis.speaking) {
         return speechSynthesis.resume();
     }
-    const utterance = new SpeechSynthesisUtterance(text);
+    if (speechSynthesis.speaking)
+        return;
+    utterance.text = text;
     utterance.rate = speedInput.value || 1;
-    utterance.addEventListener(`end`, () => {
-        textInput.disabled = false;
-    })
     textInput.disabled = true;
     speechSynthesis.speak(utterance)
 }
