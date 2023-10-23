79a259adf2236862b4f07a6080cb86defa3695cf
AlishaKausheen <alishakausheen@gmail.com>
2023-10-23 01:56:19 -0700

    pause and stop button

--- script.js
+++ script.js
@@ -7,8 +7,26 @@ const speedInput = document.getElementById(`speed`);
 playBtn.addEventListener(`click`, () => {
     playText(textInput.value);
 })
+pauseBtn.addEventListener(`click`, pauseText);
+stopBtn.addEventListener(`click`, stopText);
 function playText(text) {
+    if (speechSynthesis.paused && speechSynthesis.speaking) {
+        return speechSynthesis.resume();
+    }
     const utterance = new SpeechSynthesisUtterance(text);
-    utterance.rate = speedInput.value || 1
+    utterance.rate = speedInput.value || 1;
+    utterance.addEventListener(`end`, () => {
+        textInput.disabled = false;
+    })
+    textInput.disabled = true;
     speechSynthesis.speak(utterance)
+}
+function pauseText() {
+if (speechSynthesis.speaking) {
+        speechSynthesis.pause();
+    }    
+}
+function stopText() {
+    speechSynthesis.resume();
+    speechSynthesis.cancel();
 }
